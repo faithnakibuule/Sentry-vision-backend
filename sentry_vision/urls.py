b5f5530e890cdf72042db3618a18730a5eec5cfa
Kakyubya Misha <mishasarah198v@gmail.com>
2026-07-16 13:46:42 +0300

    Sentry vision back end url

--- sentry_vision/urls.py
+++ sentry_vision/urls.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from sentry_vision.views import AuthenticatedMediaView
 
+
+def home(request):
+    return JsonResponse({
+        "status": "ok",
+        "service": "Sentry Vision backend",
+        "api_root": "/api/",
+    })
+
 urlpatterns = [
+    path("", home),
     path("admin/", admin.site.urls),
     path("api/", include("accounts.urls")),
     path("api/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
