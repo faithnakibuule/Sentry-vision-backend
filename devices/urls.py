71a87fdfd4184ff41c218458a50f26a622300019
faith <sessangafaith@gmail.com>
2026-07-24 17:01:15 +0300

    changes to urls.py

--- devices/urls.py
+++ devices/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import DeviceHeartbeatView, DeviceProvisionView, DeviceViewSet
+from .views import DeviceHeartbeatView, DeviceProvisionView, DeviceViewSet, telemetry_view
 
 router = DefaultRouter()
 router.register("devices", DeviceViewSet, basename="device")
@@ -9,5 +9,9 @@ router.register("devices", DeviceViewSet, basename="device")
 urlpatterns = [
     path("devices/heartbeat/", DeviceHeartbeatView.as_view(), name="device-heartbeat"),
     path("devices/provision/", DeviceProvisionView.as_view(), name="device-provision"),
+    
+    # Changed from 'api/telemetry/' to 'telemetry/'
+    path("telemetry/", telemetry_view, name="telemetry"), 
+    
     *router.urls,
 ]
